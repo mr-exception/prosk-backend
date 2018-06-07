f2d2a8e7c0a749d295eecd8009cc603cba3128ee
mr-exception <darbandi1996@gmail.com>
2018-06-07 11:48:28 +0430

    token generate and check completed

---
+++ app/Http/Controllers/TokenController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+class TokenController extends Controller
+{
+    public function generate(Request $request){
+        $token = $this->generateRandomString();
+        while(true){
+            if(!User::where('token', $token)->first())
+                break;
+            else
+                $token = $this->generateRandomString();
+        }
+        $user = User::create(['token' => $token]);
+        return [
+            'ok'    => true,
+            'user'  => $user
+        ];
+    }
+
+    public function check(Request $request){
+        $user = User::where('token', $request->token)->first();
+        if($user)
+            return [
+                'ok'    => true,
+                'user'  => $user
+            ];
+        else
+            return [
+                'ok'    => false,
+                'token' => $request->token
+            ];
+    }
+
+    function generateRandomString($length = 45) {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
+}
